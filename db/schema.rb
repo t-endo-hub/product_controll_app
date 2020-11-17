b5521dab428c4cf005453b8a249e274a2a4ad631
Takumi Endo <endotaku0608@gmail.com>
2020-11-17 14:41:13 +0000

    [Add]Itemモデル、コントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_140848) do
+ActiveRecord::Schema.define(version: 2020_11_17_143836) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
