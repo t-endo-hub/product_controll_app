633669fa47413d5d1c980e8850ce0893ea32523a
Takumi Endo <endotaku0608@gmail.com>
2020-11-25 14:12:38 +0000

    [Add]担当者新規登録機能作成

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,4 +7,19 @@ class ChargesController < ApplicationController
     @charge = Charge.new
   end
   
+  def create
+    @charge = Charge.new(charge_params)
+    if @charge.save
+      flash[:notice] = "担当者を登録しました"
+      redirect_to charges_path
+    else
+      flash[:alert] = "担当者の登録に失敗しました"
+      render 'new'
+    end
+  end
+
+  private
+  def charge_params
+    params.require(:charge).permit(:charge_name)
+  end
 end
