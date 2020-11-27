c21f698306710997eb4cacce4d3672cff4b390da
Takumi Endo <endotaku0608@gmail.com>
2020-11-27 14:11:30 +0000

    [Add]バリテーション

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,2 +1,3 @@
 class Charge < ApplicationRecord
+  validates :charge_name, presence: true,  length: { maximum: 20 }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ApplicationRecord
-  validates :name, presence: true,  uniqueness: true
+  validates :name, presence: true,  length: { maximum: 20 }
 end
