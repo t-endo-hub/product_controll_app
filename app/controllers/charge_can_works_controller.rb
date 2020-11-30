dab6121b547ad6f4b06ab9869d6cc27eea17bb5d
Takumi Endo <endotaku0608@gmail.com>
2020-11-30 14:47:42 +0000

    [Add]生産可能アイテム追加機能作成

--- app/controllers/charge_can_works_controller.rb
+++ app/controllers/charge_can_works_controller.rb
@@ -1,2 +1,16 @@
 class ChargeCanWorksController < ApplicationController
+  def create
+    if @ChargeCanWork = ChargeCanWork.create(charge_can_work_params)
+      flash[:notice] = "生産可能アイテムを追加しました"
+    else
+      flash[:alert] = "生産可能アイテムの追加に失敗しました"
+    end
+    byebug
+    redirect_back(fallback_location: charge_path(Charge.find(@ChargeCanWork.charge_id)))
+  end
+
+  private
+  def charge_can_work_params
+    params.require(:charge_can_work).permit(:time_required, :item_id, :charge_id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  has_many :users, through: :charge_can_works
+  has_many :charges, through: :charge_can_works
   has_many :charge_can_works
   validates :name, presence: true,  length: { maximum: 20 }
 end
