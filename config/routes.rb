156b2af7f2524cd001d417467be6b741acc1eea9
Takumi Endo <endotaku0608@gmail.com>
2020-12-04 12:50:01 +0000

    [Fix]生産可能アイテム削除できないバグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items
   resources :charges
+  delete 'charge_can_works/:id', to: 'charge_can_works#destroy', as: 'charge_can_work'
   resources :charge_can_works
 end
