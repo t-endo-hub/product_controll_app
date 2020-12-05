0a05c3a6bea40eaabc9fca6878f166b54a5ccced
Takumi Endo <endotaku0608@gmail.com>
2020-12-05 05:39:33 +0000

    [Add]生産予定表示用の日付を取得

--- app/controllers/production_plan_on_charges_controller.rb
+++ app/controllers/production_plan_on_charges_controller.rb
@@ -1,2 +1,18 @@
 class ProductionPlanOnChargesController < ApplicationController
+  def index
+    @items = Item.all
+    # 過去１週間、先２週間の月曜日の日付を取得
+    this_day = Date.today
+    last_monday = (this_day - (this_day.wday - 1)) - 7
+    this_monday = this_day - (this_day.wday - 1)
+    next_monday = this_day - (this_day.wday - 1) + 7
+    in_2next_monday = this_day - (this_day.wday - 1) + 14
+
+    monday_arreys = [last_monday, this_monday, next_monday, in_2next_monday]
+    # フォーマットを変更して配列@mondaysに
+    @mondays = []
+    monday_arreys.each do |monday|
+      @mondays.push(monday.strftime("%Y-%m-%d"))
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'items#index'
+  root to: 'production_plan_on_charges#index'
   resources :items
   resources :charges
   delete 'charge_can_works/:id', to: 'charge_can_works#destroy', as: 'charge_can_work'
   resources :charge_can_works
+  resources :production_plan_on_charges
 end
