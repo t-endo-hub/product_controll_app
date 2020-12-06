61cf582e3f109e6e6e97a7c9700612399a9117ba
Takumi Endo <endotaku0608@gmail.com>
2020-12-06 06:09:20 +0000

    [Add]予å®/実績一覧ページ作成

--- app/controllers/production_plan_on_charges_controller.rb
+++ app/controllers/production_plan_on_charges_controller.rb
@@ -15,4 +15,23 @@ class ProductionPlanOnChargesController < ApplicationController
       @mondays.push(monday.strftime("%Y-%m-%d"))
     end
   end
+
+  def edit
+    @item_plans = ProductionPlanOnCharge.where(item_id: params[:item_id])
+    @can_work_charges = ChargeCanWork.where(item_id: params[:item_id])
+    @new_production_plan = ProductionPlanOnCharge.new
+     # 過去１週間、先２週間の月曜日の日付を取得
+     this_day = Date.today
+     last_monday = (this_day - (this_day.wday - 1)) - 7
+     this_monday = this_day - (this_day.wday - 1)
+     next_monday = this_day - (this_day.wday - 1) + 7
+     in_2next_monday = this_day - (this_day.wday - 1) + 14
+ 
+     monday_arreys = [last_monday, this_monday, next_monday, in_2next_monday]
+     # フォーマットを変更して配列@mondaysに
+     @mondays = []
+     monday_arreys.each do |monday|
+       @mondays.push(monday.strftime("%Y-%m-%d"))
+     end
+  end
 end
