fd9395ddad614be3f2a469cdc0ea3759711e716d
Takumi Endo <endotaku0608@gmail.com>
2020-12-08 00:03:39 +0000

    [Add]実績の合計数を表示

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,6 +1,7 @@
 class Charge < ApplicationRecord
-  has_many :items, through: :charge_can_works, through: :production_plan_on_charges
+  has_many :items, through: :charge_can_works, through: :production_plan_on_charges, through: :production_act_on_charges
   has_many :charge_can_works
   has_many :production_plan_on_charges, dependent: :destroy
+  has_many :production_act_on_charges, dependent: :destroy
   validates :charge_name, presence: true,  length: { maximum: 20 }
 end
--- app/models/production_act_on_charge.rb
+++ app/models/production_act_on_charge.rb
@@ -1,2 +1,4 @@
 class ProductionActOnCharge < ApplicationRecord
+  belongs_to :item
+  belongs_to :charge
 end
